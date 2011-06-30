b6323891c5f15cfc92131e29d2616e8a2e5a9987
s.huber@iwm-kmrc.de <s.huber@iwm-kmrc.de@abf22924-35f3-ca16-fbb4-1819f7593bb3>
2011-06-30 08:47:29 +0000

    Added: no keyboard version for verification tasks

--- trunk/src/com/wicam/numberlineweb/client/MathDiagnostics/VerificationTask/VerificationTaskView.java
+++ trunk/src/com/wicam/numberlineweb/client/MathDiagnostics/VerificationTask/VerificationTaskView.java
@@ -1,8 +1,12 @@
 package com.wicam.numberlineweb.client.MathDiagnostics.VerificationTask;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
 import com.wicam.numberlineweb.client.GameController;
+import com.wicam.numberlineweb.client.MathDiagnostics.MathDiagnosticsController;
 import com.wicam.numberlineweb.client.MathDiagnostics.MathDiagnosticsView;
 import com.wicam.numberlineweb.client.MathDiagnostics.isItem;
 import com.wicam.numberlineweb.client.MathDiagnostics.ChoiceReactionTask.ChoiceReactionTaskController;
@@ -11,15 +15,42 @@ public abstract class VerificationTaskView extends MathDiagnosticsView {
 
 	private final HTML itemString = new HTML();
 	private final FocusPanel focusPanel = new FocusPanel();
+	private final Image correctImage = new Image("MathDiagnostics/Apply_big.png");
+	private final Image falseImage = new Image("MathDiagnostics/Delete_big.png");
 	
 	public VerificationTaskView(int numberOfPlayers, GameController gameController) {
 		super(numberOfPlayers, gameController);
-		addFocusPanel();
 	}
 	
-	private void addFocusPanel(){
-		focusPanel.addKeyDownHandler((ChoiceReactionTaskController) gameController);
-		focusPanel.setSize("750px", "400px");
+	@Override
+	public void setHasKeyboard(boolean hasKeyboard){
+		super.setHasKeyboard(hasKeyboard);
+		if (hasKeyboard){
+			focusPanel.addKeyDownHandler((ChoiceReactionTaskController) gameController);
+			focusPanel.setSize("750px", "400px");
+		}
+		else {
+			final MathDiagnosticsController controller = (MathDiagnosticsController)gameController;
+			correctImage.addClickHandler(new ClickHandler(){
+
+				@Override
+				public void onClick(ClickEvent event) {
+					controller.onClick(event, MathDiagnosticsController.KEYRIGHTSIDE);
+					
+				}
+				
+			});
+			
+			falseImage.addClickHandler(new ClickHandler(){
+
+				@Override
+				public void onClick(ClickEvent event) {
+					controller.onClick(event, MathDiagnosticsController.KEYLEFTSIDE);
+					
+				}
+				
+			});
+		}
 	}
 
 	public void showItem (isItem item){
@@ -32,11 +63,21 @@ public abstract class VerificationTaskView extends MathDiagnosticsView {
 		String task = number1 + "&#160;" + arithmeticItem.getOperator() + "&#160;" + number2 + "&#160;=&#160;" + result;
 		itemString.setHTML("<span style='font-size:30px'>" + task + "</span>");
 		gamePanel.add(itemString);
-		gamePanel.setWidgetPosition(itemString, 310, 185);
 		
-		// add focus panel
-		gamePanel.add(focusPanel);
-		gamePanel.setWidgetPosition(focusPanel, 0, 0);
-		focusPanel.setFocus(true);
+		if (hasKeyboard){
+			gamePanel.setWidgetPosition(itemString, 310, 185);
+			
+			// add focus panel
+			gamePanel.add(focusPanel);
+			gamePanel.setWidgetPosition(focusPanel, 0, 0);
+			focusPanel.setFocus(true);
+		}
+		else {
+			gamePanel.setWidgetPosition(itemString, 310, 85);
+			gamePanel.add(correctImage);
+			gamePanel.add(falseImage);
+			gamePanel.setWidgetPosition(correctImage, 580, 260);
+			gamePanel.setWidgetPosition(falseImage, 150, 260);
+		}
 	}
 }
